2e82223a4e2717750954280aaff6e89211e10e16
muhammed umar <muhammedumaralx@gmail.com>
2023-10-11 22:30:36 +0100

    Task 1

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+method trhat calcilates the fewest number
+of operation
+"""
+
+
+def minOperations(n):
+    """funtion that Returns an integer"""
+    if n == 1:
+        return 0
+
+    operations = [0] * (n + 1)
+
+    for i in range(2, n + 1):
+        operations[i] = float("inf")
+
+        for j in range(1, i // 2 + 1):
+            if i % j == 0:
+                operations[i] = min(operations[i], operations[j] + 2 + i // j)
+
+    return operations[n] if operations[n] != float('inf') else 0
