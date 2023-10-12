592a49a8222d8a9aaf9feeb91b8539539487f603
muhammed umar <muhammedumaralx@gmail.com>
2023-10-12 21:39:49 +0100

    update

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -6,17 +6,19 @@ of operation
 
 
 def minOperations(n):
-    """funtion that Returns an integer"""
-    len_H = 1
-    len_copied_H = 0
+    """Calculates the fewest number of operations (copy, paste) needed
+    to result in exactly n H characters.
+    """
+    len_h = 1
+    len_copied_h = 0
     total_operations = 0
 
-    while len_H < n:
-        if n % len_H == 0:
+    while len_h < n:
+        if n % len_h == 0:
             total_operations += 2
-            len_copied_H = len_H
-            len_H *= 2
+            len_copied_h = len_h
+            len_h *= 2
         else:
             total_operations += 1
-            len_H += len_copied_H
+            len_h += len_copied_h
         return total_operations
