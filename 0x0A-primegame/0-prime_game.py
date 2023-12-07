59f70b1362b34293219b01ce77281a11b4f9d6b5
Muhammed Umar <112143758+muhammedumarALX@users.noreply.github.com>
2023-12-07 18:14:30 +0100

    Create 0-prime_game.py

---
+++ 0x0A-primegame/0-prime_game.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+""" Prime game """
+
+
+def isWinner(x, nums):
+    """
+        Defines the function isWinner which determines the winner of a
+        prime game session with `x` rounds.
+    """
+    if x < 1 or not nums:
+        return None
+    maria_wins = 0
+    ben_wins = 0
+    # generate primes with a limit of the maximum number in nums
+    n = max(nums)
+    primes = [True for _ in range(1, n + 1, 1)]
+    primes[0] = False
+    for i, is_prime in enumerate(primes, 1):
+        if i == 1 or not is_prime:
+            continue
+        for j in range(i + i, n + 1, i):
+            primes[j - 1] = False
+    # filter the number of primes less than n in nums for each round
+    for _, n in zip(range(x), nums):
+        primes_count = len(list(filter(lambda x: x, primes[0: n])))
+        ben_wins += primes_count % 2 == 0
+        maria_wins += primes_count % 2 == 1
+    if maria_wins == ben_wins:
+        return None
+    return 'Maria' if maria_wins > ben_wins else 'Ben'
